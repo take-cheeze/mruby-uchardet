2f220807d26589631d4c8bb5c64ed1c0bfb5ea2b
take_cheeze <takechi101010@gmail.com>
2014-06-30 22:19:16 +0900

    Add Shift_JIS test.

--- test/uchardet.rb
+++ test/uchardet.rb
@@ -5,6 +5,13 @@ assert 'UTF-8 detect' do
   assert_equal 'UTF-8', det.result
 end
 
+assert 'Shift JIS detect' do
+  det = UCharDet.new UCharDet::FILTER_ALL
+  det.handle "\x82\xb1\x82\xf1\x82\xc9\x82\xbf\x82\xed\x90\xa2\x8a\x45"
+  det.end
+  assert_equal "Shift_JIS", det.result
+end
+
 assert 'result before end' do
   det = UCharDet.new
   assert_raise(RuntimeError) { det.result }
